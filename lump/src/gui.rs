7520716e804cc88c613354ae1fdf6c2402bd5d03
bulengerkacper <bulenger.kacper@gmail.com>
2023-08-28 19:28:11 +0200

    found working guigit add *git add *git add *git add *git add *

---
+++ lump/src/gui.rs
@@ -0,0 +1,13 @@
+use dioxus::prelude::*;
+
+pub struct Gui {}
+impl Gui {
+    pub fn app(cx: Scope) -> Element {
+        let mut count = use_state(cx, || 0);
+        cx.render(rsx! {
+            h1 { "High-Five counter: {count}" }
+            button { onclick: move |_| count += 1, "Up high!" }
+            button { onclick: move |_| count -= 1, "Down low!" }
+        })
+    }
+}
--- lump/src/main.rs
+++ lump/src/main.rs
@@ -1,24 +1,22 @@
-use bevy::prelude::*;
-use std::{thread,time};
+use dioxus::prelude::*;
+use std::{thread, time};
 
 pub mod bash_connector;
-pub mod event_handler;
+pub mod gui;
+use gui::Gui;
+//pub mod event_handler;
 use bash_connector::Cache;
-use event_handler::EventHandler;
+//use event_handler::EventHandler;
 
 fn main() {
     let mut cache = Cache {
         content: String::from(""),
     };
-    
+
     let _data_collection_thread = thread::spawn(move || {
         cache.collect_data();
         thread::sleep(time::Duration::from_millis(1000));
     });
 
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Update, EventHandler::handle_mouse_events)
-        .add_systems(Update, EventHandler::handle_keyboard_events)
-        .run();
+    dioxus_desktop::launch(Gui::app);
 }
